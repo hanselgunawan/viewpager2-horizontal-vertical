3bc24ebef04a58a3c374219d1440eeb8b55590b3
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-04-14 00:28:31 -0700

    added images

--- app/src/main/java/com/hanseltritama/viewpager2horizontalvertical/MainActivity.java
+++ app/src/main/java/com/hanseltritama/viewpager2horizontalvertical/MainActivity.java
@@ -1,14 +1,20 @@
 package com.hanseltritama.viewpager2horizontalvertical;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager2 vpVertical, vpHorizontal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vpVertical = findViewById(R.id.vp_vertical);
+        vpHorizontal = findViewById(R.id.vp_horizontal);
     }
 }

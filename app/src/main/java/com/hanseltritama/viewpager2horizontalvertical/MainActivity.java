4b1e1e6fd472709425b772ede89339378f1e7404
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-04-14 00:46:52 -0700

    added adapters to the viewpagers

--- app/src/main/java/com/hanseltritama/viewpager2horizontalvertical/MainActivity.java
+++ app/src/main/java/com/hanseltritama/viewpager2horizontalvertical/MainActivity.java
@@ -1,13 +1,29 @@
 package com.hanseltritama.viewpager2horizontalvertical;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 vpVertical, vpHorizontal;
+    int[] images = {
+        R.drawable.one,
+        R.drawable.two,
+        R.drawable.three,
+        R.drawable.four,
+        R.drawable.five,
+        R.drawable.six,
+        R.drawable.seven,
+        R.drawable.eight
+    };
+
+    MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +32,38 @@ public class MainActivity extends AppCompatActivity {
 
         vpVertical = findViewById(R.id.vp_vertical);
         vpHorizontal = findViewById(R.id.vp_horizontal);
+
+        // Initialize main adapter
+        adapter = new MainAdapter(images);
+
+        // Set adapter on Vertical ViewPager
+        vpVertical.setAdapter(adapter);
+
+        // Set clip padding
+        vpHorizontal.setClipToPadding(false);
+        // Set clip children
+        vpHorizontal.setClipChildren(false);
+        // Set page limit
+        vpHorizontal.setOffscreenPageLimit(3);
+        // Set default start position
+        vpHorizontal.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
+        // Set adapter on horizontal viewpager
+        vpHorizontal.setAdapter(adapter);
+
+        CompositePageTransformer transformer = new CompositePageTransformer();
+        // Add margin between page
+        transformer.addTransformer(new MarginPageTransformer(8));
+        // Increase selected page size
+        transformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float v = 1 - Math.abs(position);
+                // Set scale y
+                page.setScaleY(0.8f + v * 0.2f);
+            }
+        });
+
+        // Set page transform
+        vpHorizontal.setPageTransformer(transformer);
     }
 }
---
+++ app/src/main/java/com/hanseltritama/viewpager2horizontalvertical/MainAdapter.java
@@ -0,0 +1,51 @@
+package com.hanseltritama.viewpager2horizontalvertical;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    int[] images;
+
+    // constructor
+    public MainAdapter(int[] images) {
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // initialize view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_main, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Set image on imageView
+        holder.imageView.setBackgroundResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.image_view);
+        }
+    }
+}
